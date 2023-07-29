597b961df6fda2686adb0e8caba784266c80981f
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2023-07-29 10:15:05 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: [process.env.ORIGIN, 'http://localhost:3000'],
+    origin: [process.env.ORIGIN],
     credentials: true
 })
 
