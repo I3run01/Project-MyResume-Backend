38a4fe7bc2d0d1946086aeaa581beef0f4a07767
I3run01 <brunnooa.v@gmail.com>
2023-07-30 09:54:08 -0300

    creating tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,20 +1,92 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { JwtService } from '@nestjs/jwt';
+import CreateUserDto from './dto/create-user.dto';
+import { User, UserDocument } from './entities/user.entity';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
+import { getModelToken } from '@nestjs/mongoose';
+import { UnauthorizedException, BadRequestException } from '@nestjs/common';
 
 describe('UsersController', () => {
-  let controller: UsersController;
+  let usersController: UsersController;
+  let usersService: UsersService;
+  let jwtService: JwtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: {
+            findByEmail: jest.fn(),
+            create: jest.fn(),
+          },
+        },
+        {
+          provide: JwtService,
+          useValue: {
+            sign: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<UsersController>(UsersController);
+    usersController = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService);
+    jwtService = module.get<JwtService>(JwtService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('signUp', () => {
+    it('should sign up a user', async () => {
+      const userDto: CreateUserDto = {
+        email: 'test@test.com',
+        password: 'password',
+        name: 'Test User',
+        avatarImage: 'test.png',
+        status: 'Active',
+      };
+
+      const hashedPassword = await bcrypt.hash(userDto.password, 10);
+      const newUser: UserDocument = { _id: 'some-id', ...userDto, password: hashedPassword } as any;
+      jest.spyOn(usersService, 'findByEmail').mockResolvedValueOnce(null);
+      jest.spyOn(usersService, 'create').mockResolvedValueOnce(newUser);
+
+      const result = await usersController.signUp(userDto, { headers: {} } as any);
+
+      expect(result).toEqual(newUser);
+    });
+
+    it('should throw BadRequestException when user already exists', async () => {
+      const userDto: CreateUserDto = {
+        email: 'test@test.com',
+        password: 'password',
+        name: 'Test User',
+        avatarImage: 'test.png',
+        status: 'Active',
+      };
+
+      const existingUser: UserDocument = { _id: 'some-id', ...userDto } as any;
+      jest.spyOn(usersService, 'findByEmail').mockResolvedValueOnce(existingUser);
+
+      await expect(usersController.signUp(userDto, { headers: {} } as any)).rejects.toThrow(BadRequestException);
+    });
+
+    it('should throw UnauthorizedException when user status is not active', async () => {
+      const userDto: CreateUserDto = {
+        email: 'test@test.com',
+        password: 'password',
+        name: 'Test User',
+        avatarImage: 'test.png',
+        status: 'Pending',
+      };
+
+      const pendingUser: UserDocument = { _id: 'some-id', ...userDto } as any;
+      jest.spyOn(usersService, 'findByEmail').mockResolvedValueOnce(pendingUser);
+
+      await expect(usersController.signUp(userDto, { headers: {} } as any)).rejects.toThrow(UnauthorizedException);
+    });
   });
 });
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -82,4 +82,85 @@ describe('UsersService', () => {
       await expect(service.findById(id)).rejects.toThrow(new NotFoundException('User not found'));
     });
   });
+
+  describe('findByEmail', () => { 
+    it('should find a user by email', async () => {
+      const email = mockUserDto.email;
+  
+      jest.spyOn(userModel, 'findOne').mockResolvedValueOnce(mockUser);
+  
+      const result = await service.findByEmail(email);
+  
+      expect(userModel.findOne).toHaveBeenCalledWith({email});
+      expect(result).toEqual(mockUser);
+    });
+  
+    it('should throw NotFoundException when user not found', async () => {
+      const email = 'nonexistent@test.com';
+  
+      jest.spyOn(userModel, 'findOne').mockResolvedValueOnce(null);
+  
+      await expect(service.findByEmail(email)).rejects.toThrow(new NotFoundException(`User with email ${email} not found`));
+    });
+  });
+
+  describe('deleteOne', () => {
+    it('should delete a user by id', async () => {
+      const id = mockUser._id.toHexString();
+  
+      jest.spyOn(userModel, 'deleteOne').mockResolvedValueOnce({ deletedCount: 1 } as any);
+  
+      const result = await service.deleteOne(id);
+  
+      expect(userModel.deleteOne).toHaveBeenCalledWith({_id: id});
+      expect(result.deletedCount).toEqual(1);
+    });
+  });
+
+  describe('updateStatus', () => {
+    it('should update a user status', async () => {
+      const id = mockUser._id.toHexString();
+      const newStatus = 'Pending';
+  
+      jest.spyOn(userModel, 'findByIdAndUpdate').mockResolvedValueOnce({ ...mockUser, status: newStatus });
+  
+      const result = await service.updateStatus(id, newStatus);
+  
+      expect(userModel.findByIdAndUpdate).toHaveBeenCalledWith(id, { status: newStatus }, { new: true });
+      expect(result.status).toEqual(newStatus);
+    });
+  
+    it('should throw NotFoundException when user not found', async () => {
+      const id = 'nonexistent-id';
+      const newStatus = 'Pending';
+  
+      jest.spyOn(userModel, 'findByIdAndUpdate').mockResolvedValueOnce(null);
+  
+      await expect(service.updateStatus(id, newStatus)).rejects.toThrow(new NotFoundException('User not found'));
+    });
+  });
+
+  describe('updatePassword', () => {
+    it('should update a user password', async () => {
+      const id = mockUser._id.toHexString();
+      const newPassword = 'newPassword';
+  
+      jest.spyOn(userModel, 'findByIdAndUpdate').mockResolvedValueOnce({ ...mockUser, password: newPassword });
+  
+      const result = await service.updatePassword(id, newPassword);
+  
+      expect(userModel.findByIdAndUpdate).toHaveBeenCalledWith(id, { password: newPassword }, { new: true });
+      expect(result.password).toEqual(newPassword);
+    });
+  
+    it('should throw NotFoundException when user not found', async () => {
+      const id = 'nonexistent-id';
+      const newPassword = 'newPassword';
+  
+      jest.spyOn(userModel, 'findByIdAndUpdate').mockResolvedValueOnce(null);
+  
+      await expect(service.updatePassword(id, newPassword)).rejects.toThrow(new NotFoundException('User not found'));
+    });
+  });
+    
 });
